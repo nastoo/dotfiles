de5b341d0d4657f22e8129718cee42734ac442a6
Nathan <nath2754@gmail.com>
2025-08-15 12:32:05 +0200

    add(nvim): kickstart now includes markview

--- dot_config/nvim/lua/custom/plugins/init.lua
+++ dot_config/nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,19 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+-- return {}
+--
+---- For `plugins/markview.lua` users.
+return {
+  'OXY2DEV/markview.nvim',
+  lazy = false,
+
+  -- For `nvim-treesitter` users.
+  priority = 49,
+
+  -- For blink.cmp's completion
+  -- source
+  -- dependencies = {
+  --     "saghen/blink.cmp"
+  -- },
+}
